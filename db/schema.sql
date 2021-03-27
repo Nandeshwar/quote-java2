fb0895bece028d296e0f14350361237e54252b6f
Sah, Nandeshwar <nandeshwar.sah@gmail.com>
2021-03-26 23:11:32 -0600

    Added docker compose file for application

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,27 @@
-DROP TABLE IF EXISTS quotedb.`hibernate_sequence`;
+#Create database
 
-CREATE TABLE quotedb.`hibernate_sequence` (
+CREATE DATABASE if NOT EXISTS quotedb;
+GRANT ALL PRIVILEGES ON quotedb.* TO 'Radha'@'%';
+FLUSH PRIVILEGES;
+
+use quotedb;
+
+CREATE TABLE IF NOT EXISTS `hibernate_sequence` (
   `next_val` bigint DEFAULT NULL
 );
 
 INSERT INTO quotedb.hibernate_sequence(next_val) VALUES (1);
 
---
--- Table structure for table `login`
---
-
-DROP TABLE IF EXISTS `login`;
-
-CREATE TABLE quotedb.`login` (
+CREATE TABLE IF NOT EXISTS `login` (
   `id` bigint NOT NULL,
   `password` varchar(255) DEFAULT NULL,
   `user` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
+insert into quotedb.login(id, user, password) values(1, 'Radha', 'Krishna');
 
-
-CREATE TABLE quotedb.`info` (
+CREATE TABLE IF NOT EXISTS `info` (
     `id` bigint PRIMARY KEY,
     `title` VARCHAR(255) NOT NULL,
     `info` VARCHAR(10000) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE quotedb.`info` (
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE quotedb.`info_link` (
+CREATE TABLE IF NOT EXISTS `info_link` (
     `id` bigint PRIMARY KEY,
     `link_id` bigint,
     `link` VARCHAR(255),
@@ -38,8 +38,7 @@ CREATE TABLE quotedb.`info_link` (
     FOREIGN KEY(link_id) REFERENCES INFO(ID)
 );
 
-insert into quotedb.login(id, user, password) values(1, 'Radha', 'Krishna');
-
+/*
 CREATE TABLE quotedb.`event_detail` (
     `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
     `day` INTEGER NOT NULL,
@@ -59,3 +58,5 @@ CREATE TABLE quotedb.`event_detail_link` (
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(link_id) REFERENCES event_detail(ID)
 );
+*/
+
